548c84f1d07179f44e304a29fae7177a82d3923f
Ladislus <walcak.ladislas@gmail.com>
2020-03-13 21:40:29 +0100

    [ADD] Added 'Cours' create/edit/delete forms

--- src/Controller/CoursController.php
+++ src/Controller/CoursController.php
@@ -4,8 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Cours;
 use App\Entity\Semestre;
+use App\Form\CoursForm;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CoursController extends AbstractController {
@@ -70,5 +73,71 @@ class CoursController extends AbstractController {
             [
                 'cours' => $cours
             ]);
+    }
+
+    /**
+     * @Route("/cours/new", name="cours_new", methods={"GET","POST"})
+     */
+    public function new_cours(Request $request) : Response
+    {
+        $cours = new Cours();
+        $form = $this->createForm(CoursForm::class, $cours);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($cours);
+            $entityManager->flush();
+
+            return $this->render('cours/cours_details.html.twig',
+                [
+                    'cours' => $cours
+                ]
+            );
+        }
+
+        return $this->render('cours/cours_new.html.twig',
+            [
+                'cours' => $cours,
+                'form' => $form->createView(),
+            ]
+        );
+    }
+
+    /**
+     * @Route("/cours/edit/{id}", name="cours_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Cours $cours): Response
+    {
+        $form = $this->createForm(CoursForm::class, $cours);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->render('cours/cours_details.html.twig',
+                [
+                    'cours' => $cours
+                ]);
+        }
+
+        return $this->render('cours/cours_edit.html.twig', [
+            'cour' => $cours,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/cours/delete/{id}", name="cours_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Cours $cours): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$cours->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($cours);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('cours');
     }
 }
---
+++ src/Form/SemestreForm.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Semestre;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SemestreForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('name')
+            ->add('pathway')
+            ->add('cours');
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Semestre::class,
+        ]);
+    }
+}
